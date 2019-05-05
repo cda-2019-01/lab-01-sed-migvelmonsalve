eaa9abbe7a5d9ba0721d3ee892c01f8c045a0a2a
Miguel Angel Monsalve Arango <50241610+migvelmonsalve@users.noreply.github.com>
2019-05-04 19:17:16 -0500

    V1

--- script.sh
+++ script.sh
@@ -1,1 +1,9 @@
 # Escriba su código aquí
+sed 's/\//-/g' data.csv > out.1
+sed 's/\;N/\;\\N/g' out.1 > out.2
+sed 's/.*/\U&/g' out.2 > out.3
+sed 's/\;\;/\;\\N\;/g' out.3 > out.4
+sed 's/\;$/\;\\N/g' out.4 > out.5
+sed 's/\,/\./g' out.5 > out.6
+sed 's/\;/\,/g' out.6 > out.7
+sed '/\\N/ d' out.7
